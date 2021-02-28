c80fe93fa4b351e00065f653f78bd977820b7f7e
ARKUDA1922 <miha-chernikov@mail.ru>
2021-02-28 13:52:21 +0300

    [upd] Домашки

--- work5/NW№2.py
+++ work5/NW№2.py
@@ -3,7 +3,7 @@
 выполнить подсчет количества строк, количества слов в каждой строке
 '''
 
-with open('test.txt', 'r') as my_f:
+with open('test2.txt', 'r') as my_f:
     lines = my_f.readlines()
     print(f"Всего строк: {len(lines)}")
     for line in lines:
